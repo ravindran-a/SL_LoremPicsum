91b32421521399f5c03af76e8ed5cc4a74f312db
Ravindran A <ravindran.abiraman@gmail.com>
2023-03-05 15:39:46 +0530

    Initial Commit

---
+++ source/LoremPicsum/LoremPicsum/LoremPicsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremPicsumApp.swift
+//  LoremPicsum
+//
+//  Created by Ravindran on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LoremPicsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
